2a3258105038b83ad773eefe0562ec5ca3bd262d
mansha28 <53634205+mansha28@users.noreply.github.com>
2019-08-06 00:26:12 +0530

    Update ll.cpp

--- ll.cpp
+++ ll.cpp
@@ -4,103 +4,111 @@ using namespace std;
 struct node{
 int x;
 int y;
-struct node *ptr;
+struct node *next;
 };
-struct node *str=NULL;
-struct node *createNode()
+struct node *start=NULL;
+
+
+void addfirst(int a,int b)
+{
+struct node *temp=new node;
+temp->x=a;
+temp->y=b;
+temp->next=NULL;
+if(start==NULL)
+{ start=temp;}
+else{temp->next=start;
+    start=temp;}
+}   
+void delfirst()
 {struct node *temp;
-	temp = (struct node *)malloc(sizeof(struct node));
-	return temp;
+temp=start;
+start=temp->next;
+delete temp;
 }
 
-void addFirst(int a, int b)
-{
-	struct node *newnode;
-	newnode = createNode();
-	if(newnode == NULL)
-	{
-		cout<<"cannot create newNode.";
-		return ;
-	}
-	newnode->x =a;
-	newnode->y=b;
-	newnode->ptr=str;
-	str=newnode;
-}
-void DelFirst(){
-struct node *temp;
-	if(str ==NULL) return;
-	else {
-		temp =str;
-		str = (str)->ptr;
-		free(temp);
-	}
-}
-void Del(int a,int b)
-{
-   struct node *temp, *prev;
-temp= str;
-while(temp->ptr != NULL)
-{prev=temp;
-if((temp->x==a)||(temp->y==b))    
-{prev->ptr=temp->ptr;
-	free(temp);
-temp=temp->ptr;
-}
-else
-temp=temp->ptr;
-}
-}
-void Search(int d)
-{
-struct node *temp;
-temp=str;
-int count=0;
-while(temp->ptr!=NULL)
-{
-if(sqrt((temp->x)*(temp->x)+(temp->y)*(temp->y))>d) temp=temp->ptr;
-else {count++; temp=temp->ptr;}
-cout<<count;
-}
+void del(int a,int b)
+{ struct node *temp,*prev;
+ temp=start;
+if (temp != NULL && (temp->x==a&&temp->y==b)) 
+    { 
+        start = temp->next;  
+        delete temp;        
+              return; 
+    } 
+
+while (temp != NULL && (temp->x==a&&temp->y==b)) 
+    { 
+        prev = temp;              
+        temp = temp->next; 
+    }
+   if (temp == NULL) return;
+   prev->next = temp->next; 
+   delete temp;
 }
 
-bool bsearch(int a, int b)
-{
-struct node *temp;
-temp = str;
-while(temp->ptr!=NULL)
-{
-if((temp->x==a)||(temp->y==b)) return true;
-else temp=temp->ptr;
+void search(float d)
+{ int x,y;
+  struct node *temp;
+  temp=start;
+  while(temp!=NULL)
+  {
+ float r=sqrt((temp->x)*(temp->x)+(temp->y)*(temp->y));
+  if(r<=d)
+ {  cout<<"("<<temp->x<<","<<temp->y<<")"; }
+  temp=temp->next;
+ }
 }
-return false;
+
+bool search(int a,int b)
+{ struct node *temp;
+  temp=start;
+    while (temp != NULL)  
+    {  
+        if (temp->x == a && temp->y == b)  
+            return true;  
+        temp = temp->next;  
+    }  
+    return false;
+
 }
-void length()
-{
-struct node *temp;
-int count=0;
-temp=str;
-while(temp->ptr!=NULL)
-{
-count++;
-temp=temp->ptr;
-cout<<count;
+
+int length()
+{struct node *temp;
+ temp=start;
+int n=0; 
+ while(temp!=NULL)
+  { n=n+1;
+   temp=temp->next;
+     }
+cout<<"lenght:"<<n;
+return 0;
 }
-}
+
 int main()
 {
 int p,m,n;
 float d;
-do
+
+
+for(int i=0; ;i++)
 {
 cin>>p;
-if(p==1) {cin>>m>>n; addFirst(m,n);}
-else if(p==2) DelFirst();
-else if(p==3) {cin>>m>>n; Del(m,n);}
-else if(p==4) { cin>>d; Search(d);}
-else if(p==5) { cin>>m>>n; bsearch(m,n);}
+if(p==0||p>6) break;
+else if(p==1) {cin>>m>>n; addfirst(m,n);}
+else if(p==2) delfirst();
+else if(p==3) {cin>>m>>n; del(m,n);}
+else if(p==4) { cin>>d; search(d);}
+else if(p==5) { cin>>m>>n; if(search(m,n)) cout<<true;
+                else cout<<false;}
 else if(p==6) length();
-return 0;
-}while((p>=1)||(p<=6));
 }
+}
+
+
+
+
+
+
+
 
